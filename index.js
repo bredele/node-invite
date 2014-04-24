1c94217885caedf59de09bea0cb1c22594d74f51
bredele <olivier.wietrich@gmail.com>
2014-04-24 13:24:58 -0600

    store hash in redis

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ Emitter(invite);
  * @api public
  */
 
-function invite(user, options) {
+function invite(user) {
 	return function(address, project) {
 		var pwd = project || user;
 		var key = crypto.createHmac("md5", pwd);
@@ -32,4 +32,13 @@ function invite(user, options) {
 		invite.emit('invite ' + user, pwd, address, hash);
 		return hash;
 	};
+}
+
+function create(user, project, address, hash) {
+	var field = 'invite:' + user;
+	var db = project ? (field + ':' + project) : field;
+	var client = redis.createClient();
+	client.hset(db, hash, project, function(err, res) {
+		console.log(res);
+	});
 }
